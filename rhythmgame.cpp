c6eff89b581698dc05b4b5620d588445f3104e46
ajatrach <aja.trachtova@gmail.com>
2023-03-08 22:44:19 -0600

    working towards demo

--- rhythmgame.cpp
+++ rhythmgame.cpp
@@ -14,32 +14,41 @@ int main() {
     // define a texture - holds graphics on GPU
     Texture textureBg;
     // load graphic on texture
-    if (textureBg.loadFromFile("assets/graphics/bg.png")) {
+    if (textureBg.loadFromFile("assets/graphics/forest.jpg")) {
         //error
     }
-    // create sprite
+    // create background sprite
     Sprite spriteBg;
     // attach texture to sprite
     spriteBg.setTexture(textureBg);
     // set spriteBg to cover screen
     spriteBg.setPosition(0, 0);
 
-    // create non-animated sprite object
+    // create 4 squares
     Texture textureBlock;
-    //textureBlock.loadFromFile("C:\\Development\\rhythmgame\\assets\\graphics\\sprite.png");
-    textureBlock.loadFromFile("assets/graphics/sprite.png");
+    textureBlock.loadFromFile("assets/graphics/square.png");
     Sprite vertBlock;
     vertBlock.setTexture(textureBlock);
-    vertBlock.setPosition(100, 560);
+    vertBlock.setPosition(100, 100);
+
+    Sprite vertBlock2;
+    vertBlock2.setTexture(textureBlock);
+    vertBlock2.setPosition(250, 100);
+
+    Sprite vertBlock3;
+    vertBlock3.setTexture(textureBlock);
+    vertBlock3.setPosition(400, 100);
+
+    Sprite vertBlock4;
+    vertBlock4.setTexture(textureBlock);
+    vertBlock4.setPosition(550, 100);
 
     // create sprite for animate object
-    Texture textureBall;
-    textureBall.loadFromFile("assets/graphics/ball.png");
-    Sprite spriteBall;
-    spriteBall.setTexture(textureBall);
-    spriteBall.setPosition(300, 760);
+    Sprite key;
+    key.setTexture(textureBlock);
+    key.setPosition(50, 20);
 
-    float ballSpeed = 0;
+    float speed = 0;
     Clock clock;
 
     // status bar for time
@@ -52,7 +61,7 @@ int main() {
 
     // manage game timer
     Time totalTime;
-    float timeRemaining = 6.0f;
+    float timeRemaining = 20.0f;
     float barWidthPerSec = barStartWidth / timeRemaining;
 
     // declare and initialise score
@@ -60,7 +69,7 @@ int main() {
 
     // reset timer and game score
     score = 0;
-    timeRemaining = 6;
+    timeRemaining = 20;
 
     // record if game is active or paused
     bool paused = true;
@@ -97,20 +106,11 @@ int main() {
     music.play();
 
     //ball initialization
-    bool ballActive = false;
+    bool keysactive = false;
 
     //input intitialization
     bool acceptInput = false;
 
-
-    // prepare the player
-    Texture texturePlayer;
-    texturePlayer.loadFromFile("assets/graphics/sprite2.png");
-    Sprite spritePlayer;
-    spritePlayer.setTexture(texturePlayer);
-    spritePlayer.setPosition(580, 720);
-    // set default posn for player to left
-    side playerSide = side::LEFT;
 
     while (window.isOpen())
     {
@@ -135,8 +135,7 @@ int main() {
                 // update user input again
                 acceptInput = true;
                 if (event.key.code == Keyboard::Up) {
-                    spritePlayer.setPosition(spritePlayer.getPosition().x,
-                        spritePlayer.getPosition().y + 20);
+                    
                 }
             }
         
@@ -180,66 +179,56 @@ int main() {
 
             // handle player input - up arrow key
             if (Keyboard::isKeyPressed(Keyboard::Up)) {
-                playerSide = side::LEFT;
-                // add time remaining whilst moving
-                while (score > 0) {
-                    timeRemaining += (2 / score) + .03;
-                }
-                spritePlayer.setPosition(spritePlayer.getPosition().x,
-                    spritePlayer.getPosition().y - 20);
                 acceptInput = false;
             }
             // check game window boundary - right edge
                 // - x value includes width of sprite obj
-            if (spritePlayer.getPosition().x > 2020) {
+            //if (spritePlayer.getPosition().x > 2020) {
                 // reset player posn to left edge of game window
                 // start sprite obj outside of window boundary to avoid sprite jumping
                 // - i.e. sprite smoothly scrolls onto window instead of
                 // automatically resetting to start of visible window
-                spritePlayer.setPosition(-40, spritePlayer.getPosition().y);
-            }
+            //    spritePlayer.setPosition(-40, spritePlayer.getPosition().y);
+           // }
             // check game window boundary - left edge
-            if (spritePlayer.getPosition().x < 0) {
+           // if (spritePlayer.getPosition().x < 0) {
                 // reset player posn to right edge of game window
-                spritePlayer.setPosition(40, spritePlayer.getPosition().y);
-            }
+            //    spritePlayer.setPosition(40, spritePlayer.getPosition().y);
+          //  }
 
 
-            // setup the ball
-            if (!ballActive) {
+            // set up the keys
+            if (!keysactive) {
                 // how fast is the ball
-                srand((int)time(0));
-                ballSpeed = (rand() % 200) + 200;
+                speed = (rand() % 200) + 200;
                 // how high is the ball
-                srand((int)time(0) * 10);
-                float height = (rand() % 500) + 500;
-                spriteBall.setPosition(2000, height);
-                ballActive = true;
+                float height = 1080;
+                key.setPosition(100, height);
+                keysactive = true;
             }
             else {
                 //value of dt.asSeconds() will be a fraction of 1
                 //represents time previous frame of animation took to complete
                 //calculation ensures rate of animation is same regardless of potential differences in underlying system performance
-                spriteBall.setPosition(
-                    spriteBall.getPosition().x - (ballSpeed * dt.asSeconds()),
-                    spriteBall.getPosition().y
-                );
+                key.setPosition(key.getPosition().x, key.getPosition().y- speed *(dt.asSeconds()));
                 // check if ball has reached left window edge
-                if (spriteBall.getPosition().x < -100) {
+                if (key.getPosition().y < -100) {
                     // reset ball ready for next frame
-                    ballActive = false;
+                    keysactive = false;
                 }
             }
 
         
             window.draw(spriteBg);
             window.draw(vertBlock);
-            window.draw(spriteBall);
+            window.draw(vertBlock);
+            window.draw(vertBlock2);
+            window.draw(vertBlock3);
+            window.draw(vertBlock4);
+            window.draw(key);
             window.draw(scoreText);
             // draw status bar
             window.draw(statusBar);
-            // draw player
-            window.draw(spritePlayer);
 
 
         }
